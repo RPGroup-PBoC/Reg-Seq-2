cf1f1fb1caf97d931c89235136f305ce935901ed
Tom Roeschinger <troeschi@caltech.edu>
2022-05-20 14:20:48 -0700

    added comments to bash script

--- code/processing/20220514_mapping/extract_promoters.sh
+++ code/processing/20220514_mapping/extract_promoters.sh
@@ -1,37 +1,54 @@
 #!/bin/bash
+
+# index for files
 group=$1
 
-
+# Find working directiory
 result=${PWD##*/}
+
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
+# Go back path
 parent_path=${parent_path%/*}
 parent_path=${parent_path%/*}
 parent_path=${parent_path%/*}
+
+# Find data directory
 folder=$parent_path'/data/processed_sequencing/'$result
+
+# Make directories for stored data
 mkdir $parent_path'/data/processed_promoters/'
 mkdir $parent_path'/data/processed_promoters/'$result
 
+# Go to data
 cd $folder
-<<comment
+
+# Sequence file names
 file_bc=$group'_R2.fastq.gz'
 file_prom=$group'_R1.fastq.gz'
 
+# Find barcodes
 gunzip -c $file_bc | awk ' NR%4==2 {
         print $0;
     }
     ' > $group'_barcodes.txt'
 
+# Find promoters
 gunzip -c $file_prom | awk ' NR%4==2 {
         print $0;
     }
     ' > $group'_promoters.txt'
-comment
 
+# Combine promoters and barcodes
 paste $group'_barcodes.txt' $group'_promoters.txt' > $group'_combined.txt'
+
+# Sort and count unique combinations
 sort $group'_combined.txt' | uniq -c | sort -bgr > $group'_collapsed.txt'
 
+# Do some formatting
+tr -s "\t" " " < $group'_collapsed.txt' > $group'_collapsed.txt'
 
+# Remove temporary files
 rm $group'_promoters.txt'
 rm $group'_barcodes.txt'
 rm $group'_combined.txt'
