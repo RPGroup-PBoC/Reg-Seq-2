a0d220a9dfdcc6dd030403fa0cda2bd454558798
Tom Roeschinger <troeschi@caltech.edu>
2022-06-13 10:29:02 -0700

    bbmap code and installation check

---
+++ check_installation.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+
+# Check if fastp is installed
+conda list -n wgregseq fastp
+
+# Check if bbmap is in the correct location
+FILE=./bbmap/bbmap.sh
+if test -f "$FILE"; then
+    echo "$FILE exists."
+else
+    echo "$FILE does not exist! Please make sure it is at the correct location."
+fi
+
+
---
+++ code/processing/20220514_mapping/run_bbmap.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# index for files
+group=$1
+
+# Find working directiory
+result=${PWD##*/}
+
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
+# Go back path
+parent_path=${parent_path%/*}
+parent_path=${parent_path%/*}
+parent_path=${parent_path%/*}
+
+# Find data directory
+folder=$parent_path'/data/processed_promoters/'$result
+
+# make key
+../../../bbmap/bbmap.sh ref=$parent_path'/data/wt_sequences.fasta'
+
+../../../bbmap/bbmap.sh ambiguous='best' indelfilter='0' nfilter='0' minid='0.85' trimreaddescriptions='t' in=$folder/$group'_collapsed.fasta' out=$folder/$group'100_collapsed.sam' t=8
