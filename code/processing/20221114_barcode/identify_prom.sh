253dc59aef7620f9243005b305262906b3b55444
Tom Roeschinger <troeschi@caltech.edu>
2022-11-16 18:07:45 -0800

    identify promoters in bash

--- code/processing/20221114_barcode/identify_prom.sh
+++ code/processing/20221114_barcode/identify_prom.sh
@@ -1,3 +1,17 @@
 gc=$1
-awk 'NR==FNR{a[$1]=$0;next} ($3 in a){b=$3;$3="";print $0 a[b]}'  "../../barcodes/20220514_mapping/mapped_barcodes.csv" "$(gc)_combined.txt" > "$(gc)_identified.txt"
-echo -e "cDNA_count gDNA_count barcode name mapping_count promoter" | cat - "$(gc)_identified.txt" > "$(gc)_identified_.txt"
+
+PARENT_PATH=$(dirname $(greadlink -f $0))
+RESULT=${PARENT_PATH##*/}
+
+# Go back path
+PARENT_PATH=${PARENT_PATH%/*}
+PARENT_PATH=${PARENT_PATH%/*}
+PARENT_PATH=${PARENT_PATH%/*}
+
+# Data FOLDER
+MAPPED=$PARENT_PATH'/data/barcodes/20220514_mapping/mapped_barcodes.csv'
+FOLDER=$PARENT_PATH'/data/processed_barcodes/'$RESULT
+
+
+awk 'NR==FNR{a[$1]=$0;next} ($3 in a){b=$3;$3="";print $0 a[b]}'  $MAPPED $FOLDER"/$(gc)_combined.txt" > $FOLDER"/$(gc)_identified.txt"
+echo -e "cDNA_count gDNA_count barcode name mapping_count promoter" | cat - $FOLDER"/$(gc)_identified.txt" > "$FOLDER/$(gc)_identified_.txt"
