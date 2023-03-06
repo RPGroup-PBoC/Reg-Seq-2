94da0d4aa3339c2b05e50f181b5764f41d39293f
Tom Roeschinger <troeschi@caltech.edu>
2023-03-06 14:34:03 -0800

    regseq processing

--- code/processing/regseq/1_1_filter_mapping_reads.sh
+++ code/processing/regseq/1_1_filter_mapping_reads.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # Find working directiory
-<<<<<<< HEAD
 DATA_FOLDER="/home/tom/git/1000_genes_ecoli/data"
 
 IN1=$DATA_FOLDER"/sequencing/SRR10971993_1.fastq"
@@ -10,14 +9,3 @@ OUT=$DATA_FOLDER"/filtered_sequencing/mapping_merged.fastq"
 HTML=$DATA_FOLDER"/mapping_fastp_report.html"
 JSON=$DATA_FOLDER"/mapping_fastp_report.json"
 fastp --in1 $IN1 --in2 $IN2 --merged_out $OUT  --verbose --html $HTML --json $JSON --report_title $html_report --thread '6' --merge --overlap_len_require '3' --n_base_limit '0' 
-=======
-DATA_FOLDER="/Volumes/rp_lab_ext/og_regseq_data"
-
-IN1="/Volumes/rp_lab_ext/og_regseq_data/SRR10971993_1.fastq"
-IN2="/Volumes/rp_lab_ext/og_regseq_data/SRR10971993_2.fastq"
-OUT="/Volumes/rp_lab_ext/og_regseq_data/mapping_merged.fastq"
-
-HTML=$DATA_FOLDER"/mapping_fastp_report.html"
-JSON=$DATA_FOLDER"/mapping_fastp_report.json"
-fastp --in1 $IN1 --in2 $IN2 --merged_out $OUT  --verbose --html $HTML --json $JSON --report_title $html_report --thread '6' --merge --overlap_len_require '1' --qualified_quality_phred '33' --n_base_limit '0' 
->>>>>>> 0994e8cb042ecb827a60dd9eb06d37c7a5029da8
--- code/processing/regseq/2_2_demultiplex_expression.sh
+++ code/processing/regseq/2_2_demultiplex_expression.sh
@@ -2,7 +2,6 @@
 
 IN=$1
 
-<<<<<<< HEAD
 DATA_FOLDER="/home/tom/git/1000_genes_ecoli/data"
 IN_PATH="/home/tom/git/1000_genes_ecoli/data/filtered_sequencing/"$IN"_barcodes.fastq"
 rm -rf $DATA_FOLDER"/"$IN"_bc_by_gc/"
@@ -40,14 +39,3 @@ for FILE in $OUT;do
     filename="${filename%.*}"
     sort --parallel 20 -T ./ $FILE | uniq -c | sort --parallel 20  -bgr -T ./|  awk -v OFS="\t" '$1=$1' > $DATA_FOLDER"/"$IN"_bc_by_gc/"$filename"_expression_counted.csv";
 done
-=======
-DATA_FOLDER="/Volumes/rp_lab_ext/og_regseq_data"
-IN_PATH="/Volumes/rp_lab_ext/og_regseq_data/filtered_sequencing/"$IN"_barcodes.fastq"
-rm -rf $DATA_FOLDER"/"$IN"_bc_by_index/"
-mkdir $DATA_FOLDER"/"$IN"_bc_by_index/"
-
-
-cat $IN_PATH | awk '/TATTAGGCTTCTCCTCAGCG/' | awk '/TCACTGGCCGTCGTTTTACATGACTGACTGA/' | awk '{ind1=substr($0, 0, 4); ind2=substr($0, 25, 4); bc=substr($0,60,20); printf "%s\t%s\t%s\n", ind1, ind2, bc}' | sort --parallel 6 | uniq -c | sort -bgr |  awk -v OFS="\t" '$1=$1' > $DATA_FOLDER"/"$IN"_expression_counted.csv"
-
-awk -v o="$DATA_FOLDER/"$IN"_bc_by_index" '{if(length($2)==4) {print $1,$3,$4 >> (o"/"$2".txt")}}' $DATA_FOLDER"/"$IN"_expression_counted.csv"
->>>>>>> 0994e8cb042ecb827a60dd9eb06d37c7a5029da8
