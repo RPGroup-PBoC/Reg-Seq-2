b97d41e353849f30f6a46642871370c1b0fef0ca
Tom <troeschi@caltech.edu>
2023-04-01 08:37:08 -0700

    delbruck update

---
+++ code/analysis/20230327_barcode/mpathic_g88.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import numpy as np
+import sys
+sys.path.insert(0, '/home/tom/mpathic')
+from mpathic import learn_model
+from multiprocessing import Pool
+df = pd.read_csv("g88_barcodes.csv")
+
+df.rename(columns={"promoter": "seq"}, inplace=True)
+
+genes = ['rspAp',
+         'araBp',
+         'znuCp',
+         'xylAp',
+         'xylFp']
+
+def run(gene):
+    db = "../../../data/mpathic_footprints/20230327_barcode/{}_g88_dataset_db".format(gene)
+    _df = df.loc[df.name == gene, :]
+    _df = _df.loc[_df.ct < 1000, :]
+    #_df = _df.loc[_df.ct_1/_df.ct > 0.02, :]
+    mcmc_df = learn_model.main(
+        df=_df,
+        lm='IM',
+        modeltype='MAT',
+        LS_means_std=None,
+        db=db,
+        iteration=600000,
+        burnin=60000,
+        thin=60,
+        runnum=0,
+        initialize='rand',
+        start=0,
+        end=None,
+        foreground=1,
+        background=0,
+        alpha=0,
+        pseudocounts=1,
+        test=False,
+        drop_library=False,
+        verbose=True,
+    )
+    mcmc_df.to_csv("footprints/LB_{}_mcmc_mpathic2.csv".format(gene))
+
+pool = Pool(6)                         
+pool.map(run, genes) 
--- code/processing/regseq/1_3_map_barcodes.jl
+++ code/processing/regseq/1_3_map_barcodes.jl
@@ -6,7 +6,6 @@ dir = @__DIR__
 home_dir = joinpath(split(dir, "/")[1:end-3])
 
 df_wt = CSV.read("/$home_dir/data/wtsequences_regseq.csv", DataFrame)
-<<<<<<< HEAD
 df_groups = CSV.read("/$home_dir/data/genetogroupnum", DataFrame, select=[2, 3])
 gene_group_dict = Dict(df_groups.genename .=> df_groups.pnum)
 
@@ -54,47 +53,4 @@ for file in readdir("/home/tom/git/1000_genes_ecoli/data/mapping_sequences"; joi
             df_out[
                 (df_out.name .== gene) .& (df_out.count .> 1), :])
     end
-=======
-
-df_out = CSV.read(
-    "/Volumes/rp_lab_ext/og_regseq_data/mapping_counted.csv", 
-    DataFrame, 
-    ignorerepeated=true,
-    delim="\t",
-    header=["counts", "promoter", "barcode"]
-    )
-
-
-
-names = String[]
-nmuts = Int[]
-promoters = String[]
-barcodes = String[]
-counts = Int[]
-gdf = groupby(df_out, :barcode)
-for _df in gdf
-    if nrow(_df) > 1
-        skip
-    end
-    x = LongDNA{4}(_df.promoter[1])
-    distances = [hamming(x, y) for y in LongDNA{4}.(df_wt.geneseq)]
-    closest_seq =  argmin(distances)
-    push!(names, df_wt.name[closest_seq])
-    push!(nmuts, minimum(distances))
-    push!(promoters, _df.promoter[1])
-    push!(barcodes, _df.barcode[1])
-    push!(counts, _df.counts[1])
-end
-
-
-df_out = DataFrame(barcode=barcodes, promoter=promoters, count=counts, name=names, nmut=nmuts)
-CSV.write("/Volumes/rp_lab_ext/og_regseq_data/mapping_identified.csv", df_out)
-display(first(df_out, 50))
-
-for gene in df_out.name |> unique
-    CSV.write(
-        "/Volumes/rp_lab_ext/og_regseq_data/promoter_bc_key_per_gene/$(gene)_identified.csv", 
-        df_out[
-            (df_out.name .== gene) .& (df_out.count .> 1), :])
->>>>>>> 0994e8cb042ecb827a60dd9eb06d37c7a5029da8
 end
